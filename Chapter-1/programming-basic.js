91965981d0209ca54cdd0419258ae6f086303afd
Mosharof <md5470415@gmail.com>
2024-10-01 08:55:02 +0600

    create a js book repo

---
+++ Chapter-1/programming-basic.js
@@ -0,0 +1,33 @@
+// Unit 1 : - Programming Basic 
+
+/* **
+* 1. Printing an out put with console.log()
+* 2. Declearing a variable : var, let and const keywords
+* 3. Statements and syntex 
+* 4. Comments 
+* 5. Operators :- [
+    * Assignment Operator
+    * Arithmatch Operator
+    * Comparative operator
+    * Logical operator
+    * typeOf,instanceOf
+]
+* 6. Data types :- [
+    * Primitive data type[
+        * Number data
+        * String data 
+        * Boolean data
+        * null data
+        * undifined 
+    * ]
+    * Non-Primitive data type / Object type [
+        * Array 
+        * Object 
+        * Function 
+        * map
+        * set
+        * weak map
+        * wea set 
+    * ]
+]
+* */
